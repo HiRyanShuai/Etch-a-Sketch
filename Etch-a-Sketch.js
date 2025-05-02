bb08da55cbd99300d085248eb05d37b132ec4d3f
Ryan Shuai <141750879+HiRyanShuai@users.noreply.github.com>
2025-05-02 20:43:29 +0930

    update creation of layout to function

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,12 +1,18 @@
 const sideContainer = 960;
-let sideNumber = 80;
-let sideLength = Math.floor(sideContainer / sideNumber);
+let sideNumber = 56;
+
 const container = document.querySelector('#container');
 
-for(let i = 0; i < sideNumber * sideNumber; i++){
-    const block = document.createElement("div");
-    block.setAttribute('class','block');
-    block.style.width = sideLength+'px';
-    block.style.height = sideLength+'px';
-    container.appendChild(block);
+function createLayout(sideCtner, sideNum){
+    let sideLength = Math.floor(sideCtner / sideNum);
+    container.replaceChildren();
+    for(let i = 0; i < sideNum * sideNum; i++){
+        const block = document.createElement("div");
+        block.setAttribute('class','block');
+        block.style.width = sideLength+'px';
+        block.style.height = sideLength+'px';
+        container.appendChild(block);
+    }    
 }
+
+createLayout(sideContainer, sideNumber);
