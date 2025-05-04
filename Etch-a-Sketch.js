667486a21a2f0079571253dc686ddd7abd04b7d3
Ryan Shuai <141750879+HiRyanShuai@users.noreply.github.com>
2025-05-04 23:37:25 +0930

    add draw mode feature(black/random/progressive) and optimise code

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,18 +1,40 @@
 const sideContainer = 960;
 let sideNumber = 96;
+let drawMode = 'black';
 
 const container = document.querySelector('#container');
+const buttons = document.querySelector('#buttons');
 const clear = document.querySelector('#clear');
 const set = document.querySelector('#set');
+const black = document.querySelector('#black');
+const random = document.querySelector('#random');
+const progressive = document.querySelector('#progressive');
 
 function draw(event){
-    event.target.style.backgroundColor = 'red';
+    switch(drawMode){
+        case 'random':
+            event.target.style.backgroundColor = 'rgb(' 
+            + Math.floor(Math.random()*256) + ' ' 
+            + Math.floor(Math.random()*256) + ' ' 
+            + Math.floor(Math.random()*256) + ')';
+            break;
+        case 'progressive':
+            console.log(event.target.style.backgroundColor);
+            event.target.style.backgroundColor === '' ?
+            event.target.style.backgroundColor = 'rgb(0 0 0 / 0.1)' :
+            event.target.style.backgroundColor = 'rgb( from ' + event.target.style.backgroundColor + ' r g b / calc(alpha + 0.1)' ;
+            break;
+        default:
+            event.target.style.backgroundColor = 'rgb(0 0 0)';
+        }
 }
 
 function createLayout(sideCtner, sideNum){
     let sideLength = Math.floor(sideCtner / sideNum);
-    container.style.width = sideLength * sideNum + 'px';
-    container.style.height = sideLength * sideNum + 'px';
+    let calSide = sideLength * sideNum + 'px';
+    container.style.width = calSide;
+    container.style.height = calSide;
+    buttons.style.width = calSide;
     container.replaceChildren();
     for(let i = 0; i < sideNum * sideNum; i++){
         const block = document.createElement("div");
@@ -28,15 +50,29 @@ createLayout(sideContainer, sideNumber);
 
 clear.addEventListener("click", (e) => {
     for (const block of container.children) {
-        block.style.backgroundColor = 'unset';
+        block.style.backgroundColor = '';
     };
 });
 
-set.addEventListener("click", ()=>{
-    let length = 0;
+set.addEventListener("click", (e)=>{
+    //let length = 0;
     do{
-        length = Number(prompt("Please input the block amount of side [1~100]:"));
-        console.log(length);
-    } while(length < 1 || length > 100 || isNaN(length) );
-    createLayout(sideContainer, length);
+        sideNumber = Math.floor(Number(prompt("Please input the number of squares per side [1~100]:")));
+    } while(sideNumber < 1 || sideNumber > 100 || isNaN(sideNumber));
+    createLayout(sideContainer, sideNumber);
+});
+
+black.addEventListener('click', (e)=>{
+    drawMode = 'black';
+    //createLayout(sideContainer, sideNumber);
+});
+
+random.addEventListener('click', (e)=>{
+    drawMode = 'random';
+    //createLayout(sideContainer, sideNumber);
+});
+
+progressive.addEventListener('click', (e)=>{
+    drawMode = 'progressive';
+    //createLayout(sideContainer, sideNumber);
 });
